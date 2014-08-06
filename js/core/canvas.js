f4dbe81a909516aab2afeaac6de9830e317acb22
Andrea Iacono <andrea.iacono.nl@gmail.com>
2014-08-06 10:28:08 +0200

    added inheritance for tree and redblacktree

--- js/core/canvas.js
+++ js/core/canvas.js
@@ -1,30 +1,33 @@
 function Canvas(tagname, canvasWidth, canvasHeight) {
     this.svg;
     this.init(tagname, canvasWidth, canvasHeight);
+
 }
 
 Canvas.prototype.init = function (tagname, canvasWidth, canvasHeight) {
-    this.svg = d3.select(tagname).append("svg")
-        .attr("id", tagname + "-canvas")
-        .style("width", canvasWidth)
-        .style("height", canvasHeight)
-        .style("background", "rgba(230, 230, 230, 0.85")
-        .style("margin", "20, auto")
-        .style("border", "2px solid #555")
-        .style("fill-opacity", "0.8");
 
-    var width = document.getElementById(tagname + "-canvas").clientWidth;
-    var height = document.getElementById(tagname + "-canvas").clientHeight;
+    if (tagname != undefined) {
+        this.svg = d3.select(tagname).append("svg")
+            .attr("id", tagname + "-canvas")
+            .style("width", canvasWidth)
+            .style("height", canvasHeight)
+            .style("background", "rgba(230, 230, 230, 0.85")
+            .style("margin", "20, auto")
+            .style("border", "2px solid #555")
+            .style("fill-opacity", "0.8");
 
-    this.svg.append("text")
-        .attr("id", tagname + "-label")
-        .attr("x", width/2.9)
-        .attr("y", height*0.95)
-        .attr("font-size", 20)
-        .attr("font-weight", "bold")
-        .style("cursor", "default")
-        .text("");
+        var width = document.getElementById(tagname + "-canvas").clientWidth;
+        var height = document.getElementById(tagname + "-canvas").clientHeight;
 
+        this.svg.append("text")
+            .attr("id", tagname + "-label")
+            .attr("x", width / 2.9)
+            .attr("y", height * 0.95)
+            .attr("font-size", 20)
+            .attr("font-weight", "bold")
+            .style("cursor", "default")
+            .text("");
+    }
 }
 
 Canvas.prototype.getCanvas = function () {
--- js/core/tree.js
+++ js/core/tree.js
@@ -34,76 +34,46 @@ Tree.prototype.createNode = function (value, color) {
 Tree.prototype.fillTree = function () {
 
     this.reset();
-    var color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
+    var color = "black";
     this.root.leftNode = new Node(this.getRandomNumber(), color, 1, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode = new Node(this.getRandomNumber(), color, 1, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode = new Node(this.getRandomNumber(), color, 2, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode = new Node(this.getRandomNumber(), color, 2, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode = new Node(this.getRandomNumber(), color, 2, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode = new Node(this.getRandomNumber(), color, 2, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode.leftNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode.leftNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode.rightNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.leftNode.rightNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode.leftNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode.leftNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode.rightNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.leftNode.rightNode.rightNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode.leftNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode.leftNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode.rightNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.leftNode.rightNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode.leftNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode.leftNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode.rightNode.leftNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
-    color = !this.isRedBlack ? "black" : Math.random() > 0.5 ? "black" : "red";
     this.root.rightNode.rightNode.rightNode.rightNode = new Node(this.getRandomNumber(), color, 3, this.isRedBlack, this.svg);
 
     this.treeHeight = this.getHeight(this.root, 0);
@@ -221,144 +191,3 @@ Tree.prototype.insertNodes = function () {
         this.insertNode();
     }
 }
-
-Tree.prototype.rotateAnimation = function () {
-
-    this.root = this.createNode("P", "black");
-    this.root.leftNode = this.createNode("A", "black");
-    this.root.rightNode = this.createNode("Q", "black");
-    this.root.rightNode.leftNode = this.createNode("B", "black");
-    this.root.rightNode.rightNode = this.createNode("C", "black");
-
-    this.treeHeight = this.getHeight(this.root, 0);
-    this.draw(false);
-    this.duration = 1500;
-    this.rotateAnimationStep1();
-}
-
-Tree.prototype.rotateAnimationStep1 = function () {
-
-    d3.select("#" + this.tagname + "-label")
-        .transition()
-        .text("Rotate Left")
-        .delay(this.duration / 2)
-        .each("end", function () {
-            treeRotate.rotateAnimationStep2();
-        });
-}
-
-Tree.prototype.rotateAnimationStep2 = function () {
-
-    var p = this.root;
-    var a = p.leftNode;
-    var q = p.rightNode;
-    var b = q.leftNode;
-    var c = q.rightNode;
-    var r = d3.select("#circle-" + p.textValue).attr("r");
-
-    p.moveTo(0, r * 1.5, null, false, this.duration);
-    a.moveTo(0, r * 1.5, p, true, this.duration);
-    q.moveTo(0, -r * 1.5, p, true, this.duration);
-    c.moveTo(0, -r * 1.5, q, true, this.duration);
-    b.moveTo(0, -r * 1.5, q, false, this.duration);
-
-    d3.select("#circle-" + p.textValue)
-        .transition()
-        .delay(this.duration)
-        .each("end", function () {
-            treeRotate.rotateAnimationStep3();
-        });
-}
-
-Tree.prototype.rotateAnimationStep3 = function () {
-
-    var p = this.root;
-    var a = p.leftNode;
-    var q = p.rightNode;
-    var b = q.leftNode;
-    var c = q.rightNode;
-    var r = d3.select("#circle-" + p.textValue).attr("r");
-
-    this.root = q;
-    q.rightNode = c;
-    q.leftNode = p;
-    p.leftNode = a;
-    p.rightNode = b;
-
-    p.moveTo(-r / 2, r * 1.5, q, true, this.duration);
-    q.moveTo(0, -r * 1.5, p, true, this.duration);
-    c.moveTo(r * 1.5, -r * 1.5, q, true, this.duration);
-    b.moveTo(0, r * 1.5, p, true, this.duration);
-    a.moveTo(0, r * 1.5, p, true, this.duration);
-
-    d3.select("#" + this.tagname + "-label")
-        .transition()
-        .text("")
-        .delay(this.duration)
-        .each("end", function () {
-            treeRotate.rotateAnimationStep4();
-        });
-}
-
-Tree.prototype.rotateAnimationStep4 = function () {
-
-    d3.select("#" + this.tagname + "-label")
-        .transition()
-        .text("Rotate Right")
-        .delay(this.duration)
-        .each("end", function () {
-            treeRotate.rotateAnimationStep5();
-        });
-}
-
-Tree.prototype.rotateAnimationStep5 = function () {
-
-    var q = this.root;
-    var p = q.leftNode;
-    var a = p.leftNode;
-    var b = p.rightNode;
-    var c = q.rightNode;
-    var r = d3.select("#circle-" + p.textValue).attr("r");
-
-    this.root = p;
-    p.rightNode = q;
-    q.leftNode = b;
-
-    p.moveTo(0, -r * 1.5, null, false, this.duration);
-    a.moveTo(0, -r * 1.5, p, true, this.duration);
-    q.moveTo(0, r * 1.5, p, true, this.duration);
-    c.moveTo(0, r * 1.5, q, true, this.duration);
-    b.moveTo(0, -r * 1.5, q, false, this.duration);
-
-    d3.select("#circle-" + p.textValue)
-        .transition()
-        .delay(this.duration)
-        .each("end", function () {
-            treeRotate.rotateAnimationStep6();
-        });
-}
-
-Tree.prototype.rotateAnimationStep6 = function () {
-
-    var p = this.root;
-    var a = p.leftNode;
-    var q = p.rightNode;
-    var b = q.leftNode;
-    var c = q.rightNode;
-    var r = d3.select("#circle-" + p.textValue).attr("r");
-
-    p.moveTo(r / 2, -r * 1.5, q, true, this.duration);
-    q.moveTo(0, r * 1.5, p, true, this.duration);
-    c.moveTo(-r * 1.5, r * 1.5, q, true, this.duration);
-    b.moveTo(0, r * 1.5, q, true, this.duration);
-    a.moveTo(0, -r * 1.5, p, true, this.duration);
-
-    d3.select("#" + this.tagname + "-label")
-        .transition()
-        .text("")
-        .delay(this.duration)
-        .each("end", function () {
-            treeRotate.rotateAnimationStep1();
-        });
-}
-
